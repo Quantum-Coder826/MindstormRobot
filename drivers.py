2fecec2560b6118651704dc19f0bd5014f43e74e
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-10 11:53:43 +0200

    device conection code

--- drivers.py
+++ drivers.py
@@ -24,3 +24,29 @@ def writeFile(path, data):
     f = open(path, "w")
     f.write(data)
     f.close()
+
+# todo find all sensors and motors (use a class)
+class brick:
+    def __init__(ev3):
+        pass
+
+    def configure():
+        for dir in listdir(rootmotor): #find all avalable motors and save the data
+            devices[readFile(rootmotor + str(dir) + "/address")] = {
+                "path": str(rootmotor + str(dir)),
+                "driver_name": str(readFile(rootmotor + str(dir) + "/driver_name")),
+                "count_per_rot": str(readFile(rootmotor + str(dir) + "/count_per_rot")),
+                "max_speed": str(readFile(rootmotor + str(dir) + "/max_speed"))
+            }
+        
+        for dir in listdir(rootsensor): #inf all avalable sensor and save the data
+            devices[readFile(rootsensor + str(dir) + "/address")] + {
+                "path": str(rootsensor + str(dir)),
+                "driver_name": str(rootsensor + str(dir) + "/driver_name"),
+                "decimals": str(rootsensor + str(dir) + "/decimals"),
+                "num_values": str(rootsensor + str(dir) + "/num_values")
+            }
+
+
+# todo make a class to read/control motors
+# todo make a class to read/contorl sensors
