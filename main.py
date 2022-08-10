e807eb039b6b7789e7bebf6facb2b3f504d96a1f
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-10 15:59:05 +0200

    tank drive testing

--- main.py
+++ main.py
@@ -3,19 +3,33 @@
 
 from drivers.brick import *
 from drivers.sensor import *
+from drivers.motor import *
 
 from os import listdir
 from time import sleep
 import sys
 
+print("ready")
+
+keys = [(0,0),(50,0),(-50,0),(0,50),(0,-50),(50,50),(50,-50),(-50,50),(-50,-50),(0,0),(0,0),(0,0)]
+
 brick.configure()
 
-US = sensor(ports.in4)
-US.setMode("US-DIST-CM")
+IR = sensor(ports.in4)
+IR.setMode("IR-REMOTE")
+
+rightMotor = motor(ports.outA)
+leftMotor = motor(ports.outD)
+rightMotor.setStopAction("coast")
+leftMotor.setStopAction("coast")
 
 try:
     while True:
-        print(str(US.getValue()) + " CM")
+        key = IR.getValue()
+        rightMotor.runCommand("run-direct", duty=keys[key][0])
+        leftMotor.runCommand("run-direct", duty=keys[key][1])
+
+
 except KeyboardInterrupt:
     print("exiting")
     brick.resetAll()
