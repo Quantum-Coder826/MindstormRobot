7edc8fd2215f04b737e14094ff220c2353539d59
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-10 13:38:08 +0200

    motor control

--- drivers.py
+++ drivers.py
@@ -9,7 +9,7 @@
 from os import listdir
 
 rootmotor = "/sys/class/tacho-motor"
-rootsensor = "/sys/class/lego/sensor"
+rootsensor = "/sys/class/lego-sensor"
 devices = {}
 
 # general functions
@@ -25,28 +25,59 @@ def writeFile(path, data):
     f.write(data)
     f.close()
 
-# todo find all sensors and motors (use a class)
+
 class brick:
     def __init__(ev3):
         pass
 
-    def configure():
+    def configure(): # finds all connectet devices and create defaults
         for dir in listdir(rootmotor): #find all avalable motors and save the data
-            devices[readFile(rootmotor + str(dir) + "/address")] = {
-                "path": str(rootmotor + str(dir)),
-                "driver_name": str(readFile(rootmotor + str(dir) + "/driver_name")),
-                "count_per_rot": str(readFile(rootmotor + str(dir) + "/count_per_rot")),
-                "max_speed": str(readFile(rootmotor + str(dir) + "/max_speed"))
+            devices[readFile(rootmotor + "/" + str(dir) + "/address")] = {
+                "path": str(rootmotor + "/" +str(dir)),
+                "driver_name": str(readFile(rootmotor + "/" + str(dir) + "/driver_name")),
+                "count_per_rot": str(readFile(rootmotor + "/" + str(dir) + "/count_per_rot")),
+                "max_speed": str(readFile(rootmotor + "/" + str(dir) + "/max_speed"))
             }
         
         for dir in listdir(rootsensor): #inf all avalable sensor and save the data
-            devices[readFile(rootsensor + str(dir) + "/address")] + {
-                "path": str(rootsensor + str(dir)),
-                "driver_name": str(rootsensor + str(dir) + "/driver_name"),
-                "decimals": str(rootsensor + str(dir) + "/decimals"),
-                "num_values": str(rootsensor + str(dir) + "/num_values")
+            devices[readFile(rootsensor + "/" + str(dir) + "/address")] = {
+                "path": str(rootsensor + "/" + str(dir)),
+                "driver_name": str(rootsensor + "/" + str(dir) + "/driver_name"),
+                "decimals": str(rootsensor + "/" + str(dir) + "/decimals"),
+                "num_values": str(rootsensor + "/" + str(dir) + "/num_values")
             }
 
+# class to control a motor
+class motor:
+    def __init__(lego_motor, port = "lego-ports:OutA"):
+        lego_motor.port = "logo-ports:Out" + str(port)
+    
+    def getState(lego_motor):
+        return readFile(devices[lego_motor.port]["path"] + "/state")
 
-# todo make a class to read/control motors
+    def getSpeed(lego_motor):
+        return readFile(devices[lego_motor.port]["path"] + "/speed")
+
+    def runCommand(lego_motor, command, speed = 0, angle = 0, time = 0, duty = 0):
+        writeFile(devices[lego_motor.port]["path"] + "/speed_sp", speed)
+        writeFile(devices[lego_motor.port]["path"] + "/position_sp", angle)
+        writeFile(devices[lego_motor.port]["path"] + "/time_sp", time)
+        writeFile(devices[lego_motor.port]["path"] + "/duty_cycle_sp", duty)
+        writeFile(devices[lego_motor.port]["paht"] + "/command", command)
+    
+    def stop(lego_motor):
+        try: 
+            writeFile(devices[lego_motor.port]["path"] + "/command", "stop")
+            return True
+        except: return False
+    
+    def reset(lego_motor):
+        try:
+            writeFile(devices[lego_motor.port]["path"] + "/command", "reset")
+            return True
+        except: return False
+    
+
+
+
 # todo make a class to read/contorl sensors
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-# * python3 /home/robot/robot/robot.py
+# * python3 /home/robot/MindstormsRobot/main.py
 
 from drivers import *
 from os import listdir
 import sys
 
+brick.configure() # config the brick
+
 try:
     while True:
-        empty = 0
-        stuff.
+        print(devices)
 except KeyboardInterrupt:
     print("exiting")
     sys.exit(0)
