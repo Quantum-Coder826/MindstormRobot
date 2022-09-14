6c8005d763387f7356f96558f5776c454f3b52d2
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-14 17:33:50 +0200

    and doing it agen

--- robot/motor.py
+++ robot/motor.py
@@ -17,36 +17,3 @@ class motor:
     def __init__(lego_motor, address = "ev3-ports:outA"):
         lego_motor.address = address
     
-    def setDuty(lego_motor, duty = 0):
-        writeFile(devices[lego_motor.address]["path"] + "/duty_cycle_sp", duty)
-    
-    def setPostion(lego_motor, angle = 0):
-        writeFile(devices[lego_motor.address]["path"] + "/postion_sp", angle)
-    
-    def setSpeed(lego_motor, speed = 0):
-        writeFile(devices[lego_motor.address]["path"] + "/speed_sp", speed)
-
-    def getState(lego_motor):
-        return readFile(devices[lego_motor.address]["path"] + "/state")
-    
-    def setStopAction(lego_motor, action):
-        writeFile(devices[lego_motor.address]["path"] + "/stop_action", action)
-    
-    def stopMotor(lego_motor):
-        writeFile(devices[lego_motor.address]["path"] + "/command", "stop")
-    
-    def runAbs(lego_motor, angle = 0):
-        writeFile(devices[lego_motor.address]["path"] + "/postions_sp", angle)
-        writeFile(devices[lego_motor.address]["path"] + "/command", "run-to-abs-pos")
-    
-    def runRel(lego_motor, angle = 0):
-        motor.setPostion(lego_motor.address, angle)
-        writeFile(devices[lego_motor.address]["path"] + "/command", "run-to-rel-pos")
-    
-    def runTimed(lego_motor, time = 0):
-        writeFile(devices[lego_motor.address]["path"] + "/time_sp", time)
-        writeFile(devices[lego_motor.address]["path"] + "/command", "run-timed")
-    
-    def runDirect(lego_motor, duty = 0):
-        motor.setDuty(lego_motor.address, duty)
-        writeFile(devices[lego_motor.address]["path"] + "/command", "rund-direct")
