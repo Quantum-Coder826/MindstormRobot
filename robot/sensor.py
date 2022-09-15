ff05010fb48945d21a87022e01c7bf078dd5044c
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-15 21:03:57 +0200

    sensor mode setting

--- robot/sensor.py
+++ robot/sensor.py
@@ -1,5 +1,16 @@
 from robot.brick import writeFile, readFile, devices
 
+def setMode(port, mode):
+    if mode not in readFile(atribute(port, "modes")):
+        raise Exception("The mode: " + mode + " is not avalabe for device on: " + port + "\navalable modes are: " + readFile(atribute(port, "modes")))
+    if mode == readFile(atribute(port, "mode")):
+        return True
+    writeFile(atribute(port, "mode"), mode)
+
+def atribute(port, file):
+    return devices[port]["path"] + file
+
 class sensor:
     def __init__(lego_sensor, address = "ev3-ports:in1"):
         lego_sensor.address = address
+        
