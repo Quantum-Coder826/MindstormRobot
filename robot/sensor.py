a0e08d5546f05667ca0dd82ea49e59b640d0ae16
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-15 20:49:08 +0200

    re doing sensor

--- robot/sensor.py
+++ robot/sensor.py
@@ -3,28 +3,3 @@ from robot.brick import writeFile, readFile, devices
 class sensor:
     def __init__(lego_sensor, address = "ev3-ports:in1"):
         lego_sensor.address = address
-    
-    @staticmethod
-    def reload(target): #reloads a sensor attached to a given port
-        devices[target]["decimals"] = readFile(devices[target]["path"] + "/decimals")
-        devices[target]["num_values"] = readFile(devices[target]["path"] + "/num_values")
-
-    def setMode(lego_sensor, mode):
-        writeFile(devices[lego_sensor.address]["path"] + "/mode", mode)
-        sensor.reload(lego_sensor.address)
-
-    def getValue(lego_sensor, value = 0):
-        data = int(readFile(devices[lego_sensor.address]["path"] + "/value" + str(value)))
-        if devices[lego_sensor.address]["decimals"] == "1":
-            return data / 10
-        else:
-            return data
-    
-    def getUnit(lego_sensor):
-        return readFile(devices[lego_sensor.address]["path"] + "/units")
-    
-    def isPressed(lego_sensor):
-        if readFile(devices[lego_sensor.address]["path"] + "/value0") == 1:
-            return True
-        if readFile(devices[lego_sensor.address]["paht"] + "/value0") == 0:
-            return False
