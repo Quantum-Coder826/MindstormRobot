9a06fa9068b712c9c9fb1a4b3421dcc4c9e2c6c4
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-15 21:08:48 +0200

    added file for all constantes

--- robot/brick.py
+++ robot/brick.py
@@ -1,4 +1,5 @@
 from os import listdir
+from robot.defaults import ports, led
 
 motorpath = "/sys/class/tacho-motor"
 sensorpath = "/sys/class/lego-sensor"
@@ -51,22 +52,3 @@ class mindstroms:
         writeFile(ledpath[target] + "/brightness", value)
 
     # todo: figure out how the hell to read the buttons (https://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/ev3.html#buttons)
-
-class ports:
-    # all the ports
-    outA = "ev3-ports:outA"
-    outB = "ev3-ports:outB"
-    outC = "ev3-ports:outC"
-    outD = "ev3-ports:outD"
-
-    in1 = "ev3-ports:in1"
-    in2 = "ev3-ports:in2"
-    in3 = "ev3-ports:in3"
-    in4 = "ev3-ports:in4"
-
-class led:
-    # led codes
-    leftGreen = 0
-    leftRed = 1
-    rightGreen = 2
-    rightRed = 3
