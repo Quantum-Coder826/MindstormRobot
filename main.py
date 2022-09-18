3d56d7e18d05e265596554848685cc6db4201612
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-18 19:19:37 +0200

    cleanup

--- main.py
+++ main.py
@@ -5,23 +5,3 @@ print("running")
 
 map = [(0,0),(50,0),(-50,0),(0,50),(0,-50),(50,50),(50,-50),(-50,50),(-50,-50),(0,0),(0,0),(0,0)]
 
-brick = mindstroms()
-
-rightMotor = motor(ports.outD)
-leftMotor = motor(ports.outA)
-IR = sensor(ports.in4)
-
-rightMotor.runDuty(0)
-leftMotor.runDuty(0)
-IR.setMode("IR-REMOTE")
-
-try:
-    while True:
-        key = IR.getValue()
-        rightMotor.setDuty(map[key][0])
-        leftMotor.setDuty(map[key][1])
-
-except KeyboardInterrupt:
-    print("exiting")
-    brick.resetAll()
-    exit()
