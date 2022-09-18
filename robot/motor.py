3ef03a8682980f3a909bf55b858efc389f21c6a5
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-18 19:21:49 +0200

    more cleanup

--- robot/motor.py
+++ robot/motor.py
@@ -1,8 +1,3 @@
-"""
-This file was made by Berend Veldthuis
-the code is respolable for controlling lego motors 45502 and 2550113
-The code is based on ev3dev: https://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/motors.html#sysfs
-"""
 from robot.brick import writeFile, readFile, devices
 
 class motor:
