5f57953a1a6b024dac4a75ac842ac82b8ac3d938
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-18 19:46:05 +0200

    compleated sensor

--- robot/sensor.py
+++ robot/sensor.py
@@ -8,10 +8,27 @@ class sensor:
         devices[lego_sensor.address]["modes"] = readFile(devices[lego_sensor.address]["path"] + "/modes")
     
     def getRaw(lego_sensor):
-        binData = readFile(devices[lego_sensor.address]["path"] + "/bin_data")
+        file = open(devices[lego_sensor.address]["path"] + "bin_data", "rb")
+        binData = file.read()
+        del file
         binDataFormat = readFile(devices[lego_sensor.address]["path"] + "/bin_data_format")
         return binData, binDataFormat
     
-    def sendcommand(lego_sensor, command):
-        if command not in devices[lego_sensor.address]["modes"]:
-            raise Exception("The mode: " + str(command) + " is not avalable for device: " + str(lego_sensor.address) + "accepted modes are: " + str(devices[lego_sensor.address]["modes"]))
+    def setMode(lego_sensor, mode):
+        if mode not in devices[lego_sensor.address]["modes"]:
+            raise Exception("The mode: " + str(mode) + " is not avalable for device: " + str(lego_sensor.address) + "accepted modes are: " + str(devices[lego_sensor.address]["modes"]))
+        writeFile(devices[lego_sensor.address]["path"] + "/mode", mode)
+        devices[lego_sensor.address]["decimals"] = int(readFile(devices[lego_sensor.address]["path"] + "/decimals"))
+        devices[lego_sensor.address]["num_values"] = int(readFile(devices[lego_sensor.address]["path"] + "/num_values"))
+
+    def getUnits(lego_sensor):
+        return readFile(devices[lego_sensor.address]["path"] + "/units")
+    
+    def getValue(lego_sensor, value = 0):
+        if value > devices[lego_sensor.address]["num_values"]:
+            raise Exception("You tried to get a value that is not avalable try again. port: " + str(lego_sensor.address) + " mode: " + str(readFile(devices[lego_sensor.address]["path"] + "/mode")))
+        value = int(readFile(devices[lego_sensor.address]["path"] + "/value" + str(value)))
+        if devices[lego_sensor.address]["decimals"] == 0:
+            return value
+        value = value / 10
+        return value
