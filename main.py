d499712913fcc816e5d9765dcebb6aca0ac461ef
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-19 16:07:15 +0200

    cleanup

--- main.py
+++ main.py
@@ -4,13 +4,3 @@ from robot.sensor import *
 map = [(0,0),(50,0),(-50,0),(0,50),(0,-50),(50,50),(50,-50),(-50,50),(-50,-50),(0,0),(0,0),(0,0)]
 
 brick = mindstorms()
-
-button = sensor(ports.in1)
-
-while True:
-    if button.isPressed():
-        break
-    else:
-        brick.setLed(0,0)
-
-brick.setLed(1,255)
