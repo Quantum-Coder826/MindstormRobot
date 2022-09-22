c94f4814fc108ef07c480d2918f6abebd28ef928
Berend Veldthuis <57870011+Quantum-Coder826@users.noreply.github.com>
2022-09-22 13:08:20 +0200

    driver stuff

--- robot/motor.py
+++ robot/motor.py
@@ -10,6 +10,37 @@ class motor:
             if port == readFile("/sys/class/tacho-motor/" + str(dir) + "/address"):
                 self.path = "/sys/class/tacho-motor/" + str(dir)
 
+    def sendCommand(self, command):
+        if command not in readFile(self.path + "/commands"):
+            raise Exception("The command: " + command + " is not available for: " + str(self.path))
+        writeFile(self.path + "command", str(command))
+    
+    def getCountPerRot(self):
+        return int(readFile(self.path + "/count_per_rot"))
+    
+    def getCountPerMeter(self):
+        return int(readFile(self.path + "/count_per_m"))
+    
+    def getFullTravel(self):
+        return int(readFile(self.path + "/full_travel_count"))
+    
+    def driver(self):
+        return readFile(self.path + "/driver_name")
+    
+    def setDuty(self, duty = 0):
+        writeFile(self.path + "/duty_cycle_sp", int(duty))
+    
     def getPosition(self):
         return int(readFile(self.path + "/position"))
+
+    def maxSpeed(self):
+        return int(readFile(self.path + "/max_speed"))
+    
+    def setPosition(self, angle = 0):
+        writeFile(self.path + "/position_sp", int(angle))
+    
+    def speed(self):
+        return int(readFile(self.path + "/speed"))
+        
+
 
