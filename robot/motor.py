4b2882f25855f2d97e685bd70cc133ab8f05034f
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-09-22 17:26:10 +0200

    name change

--- robot/motor.py
+++ robot/motor.py
@@ -4,43 +4,43 @@ from robot.brick import writeFile, readFile
 from os import listdir
 
 class motor:
-    def __init__(self, port):
-        self.port = port
+    def __init__(lego_motor, port):
+        lego_motor.port = port
         for dir in listdir("/sys/class/tacho-motor"):
             if port == readFile("/sys/class/tacho-motor/" + str(dir) + "/address"):
-                self.path = "/sys/class/tacho-motor/" + str(dir)
+                lego_motor.path = "/sys/class/tacho-motor/" + str(dir)
 
-    def sendCommand(self, command):
-        if command not in readFile(self.path + "/commands"):
-            raise Exception("The command: " + command + " is not available for: " + str(self.path))
-        writeFile(self.path + "command", str(command))
+    def sendCommand(lego_motor, command):
+        if command not in readFile(lego_motor.path + "/commands"):
+            raise Exception("The command: " + command + " is not available for: " + str(lego_motor.path))
+        writeFile(lego_motor.path + "command", str(command))
     
-    def getCountPerRot(self):
-        return int(readFile(self.path + "/count_per_rot"))
+    def getCountPerRot(lego_motor):
+        return int(readFile(lego_motor.path + "/count_per_rot"))
     
-    def getCountPerMeter(self):
-        return int(readFile(self.path + "/count_per_m"))
+    def getCountPerMeter(lego_motor):
+        return int(readFile(lego_motor.path + "/count_per_m"))
     
-    def getFullTravel(self):
-        return int(readFile(self.path + "/full_travel_count"))
+    def getFullTravel(lego_motor):
+        return int(readFile(lego_motor.path + "/full_travel_count"))
     
-    def driver(self):
-        return readFile(self.path + "/driver_name")
+    def driver(lego_motor):
+        return readFile(lego_motor.path + "/driver_name")
     
-    def setDuty(self, duty = 0):
-        writeFile(self.path + "/duty_cycle_sp", int(duty))
+    def setDuty(lego_motor, duty = 0):
+        writeFile(lego_motor.path + "/duty_cycle_sp", int(duty))
     
-    def getPosition(self):
-        return int(readFile(self.path + "/position"))
+    def getPosition(lego_motor):
+        return int(readFile(lego_motor.path + "/position"))
 
-    def maxSpeed(self):
-        return int(readFile(self.path + "/max_speed"))
+    def maxSpeed(lego_motor):
+        return int(readFile(lego_motor.path + "/max_speed"))
     
-    def setPosition(self, angle = 0):
-        writeFile(self.path + "/position_sp", int(angle))
+    def setPosition(lego_motor, angle = 0):
+        writeFile(lego_motor.path + "/position_sp", int(angle))
     
-    def speed(self):
-        return int(readFile(self.path + "/speed"))
+    def speed(lego_motor):
+        return int(readFile(lego_motor.path + "/speed"))
         
 
 
