7241c383aeaa824718cd6f625e10b05555f1b5a6
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-10-11 16:50:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,18 @@ from robot.brick import *
 from robot.motor import *
 from robot.sensor import *
 
-keyMap = [(0,0),(50,0),(-50,0),(0,50),(0,-50),(50,50),(50,-50),(-50,50),(-50,-50),(0,0),(0,0),(0,0)]
-
 brick = mindstorms()
-for i in range(0,3):
-    brick.setLed(i,0)
 
 leftMotor = motor(ports.outB)
 rightMotor = motor(ports.outC)
 leftMotor.sendCommand("run-direct")
 rightMotor.sendCommand("run-direct")
 
-IR = sensor(ports.in4)
-IR.setMode("IR-REMOTE")
+sonar = sensor(ports.in4)
+sonar.setMode("US-DIST-CM")
 
 while True:
-    key = IR.getValue()
-    if key == 10:
-        brick.setLed(0,255)
-        brick.setLed(2,255)
-    elif key == 11:
-        brick.setLed(1,255)
-        brick.setLed(3,255)
+    if sonar.getValue() <= 13:
+        leftMotor.setDuty(0)
     else:
-        for i in range(0,4):
-            brick.setLed(i,0)
-    leftMotor.setDuty(keyMap[key][0])
-    rightMotor.setDuty(keyMap[key][1])
+        leftMotor.setDuty(50)
