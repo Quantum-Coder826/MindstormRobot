93d74b5950e1ae81ed1d48ccec5f2dbd1f202c09
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-02-27 16:46:21 +0100

    added brick.rs

--- src/brick.rs
+++ src/brick.rs
@@ -2,13 +2,16 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
+// function for interacting with sysfs.
 pub fn read_from_file(path: &str) -> String { // read from file path and return as String.
     let buffer: String = fs::read_to_string(path).expect("failed to read file.");
     return buffer;
 }
 
-pub fn write_to_file(path: &str, buffer: &[u8]) -> std::io::Result<()> {
+pub fn write_to_file(path: &str, buffer: &[u8]) -> std::io::Result<()> { // write string to a file path
     let mut file = File::create(path)?;
     file.write_all(buffer)?;
     Ok(())
 }
+
+// functions for interacting with the brick hardware.
