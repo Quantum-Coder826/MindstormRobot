0a4e5f7c5ebeb33107504de9fe00ba71ba9d17ae
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-02-27 17:56:22 +0100

    update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 mod brick;
 
 fn main() {
-    brick::write_to_file("/sys/class/tacho-motor/motor0/command", b"reset");
-
-    while true {
-        let pos:String = brick::read_from_file("/sys/class/tacho-motor/motor0/position");
-        println!("{pos}");
-    }
+    println!("hello");
 }
