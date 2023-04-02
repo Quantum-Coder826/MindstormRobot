17bf2ee5beb47c33a5f4523e6ef2892a1a8edfdf
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-04-02 19:53:57 +0200

    hopefuly functionig device discorvery

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod robot;
+use robot::motor::Motor;
+use robot::brick;
+
 fn main() {
-    
-    
+       let leftMotor = Motor.attatch("lego-ev3:outA");
+
 }
--- src/robot/motor.rs
+++ src/robot/motor.rs
@@ -1,1 +1,30 @@
+use crate::robot::brick;
+use std::io::{Error, Read};
+use std::fs::{read_dir, ReadDir};
 
+
+pub struct Motor {
+    port: String,
+    path: String
+}
+
+impl Motor {
+    pub fn attatch(port: &str) -> Self {
+        Motor { 
+            port: port.to_string(), 
+            path: get_sysfs_path(port)
+        }}
+    }
+
+    fn get_sysfs_path(port: &str) -> String {
+        let paths = read_dir("/sys/class/tacho-motor/").unwrap();
+        
+        for path in paths {
+            let test_path = path.unwrap().path().display().to_string() + "address";
+            if brick::file_read_string(&test_path) == port {
+                return path.unwrap().path().display().to_string();
+            }
+        }
+        panic!("Could not find any devices on port: {:?}", port) // panic cuz cannot find a device that should be connected
+
+    }
