4c7ff9cfc9498063683a6a1cf608fe689741afbf
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-04-03 09:39:49 +0200

    added todo

--- src/robot/motor.rs
+++ src/robot/motor.rs
@@ -17,6 +17,7 @@ impl Motor {
     // methods for initializing class
     pub fn attatch(port: &str) -> Motor {
         let path_ret: String = Self::get_sysfs_path(port); // NOTE: need to use the .clone() method to copy the value so we don't get a 'borrow of moved value' error
+        brick::write_str_file(&(path_ret.clone() + "/command"), "reset");
 
         Motor {
             port: port.to_string(),
@@ -41,5 +42,12 @@ impl Motor {
     }
 
     // methods for controlling the tachio-motors
+    /* list of needed methods
+    run_direct() control the motor using the duty cycle
+    run_to_rel(angle, speed) run the motor to a relative specificed angel
+    run_to_abs(angle, speed) run the motor to a absolute angle
+    run_timed(time, speed, stop_action) run the motor for the specificd time and use the preset stopaction
+    stop(stop_action) stops the motor using the stop action
+    reset() resets all values related to the motor */
     
 }
