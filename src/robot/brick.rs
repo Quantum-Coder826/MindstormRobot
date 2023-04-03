8b9b2888d189de9cdb88dca7d5e6b090b0e3c818
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-04-03 15:31:46 +0200

    added led control

--- src/robot/brick.rs
+++ src/robot/brick.rs
@@ -1,15 +1,20 @@
 use crate::robot::files;
 
 // these function will handel the ev3 brick hardware
+#[allow(dead_code)]
+static  LED_PATHS:[&str; 4] = ["/sys/class/leds/led0:green:brick-status/brightness", "/sys/class/leds/led0:red:brick-status/brightness", "/sys/class/leds/led1:green:brick-status/brightness", "/sys/class/leds/led1:red:brick-status/brightness"];
 
 // TODO: add functions to read buttons
 
 // functions for controlling the LED's
 pub fn clear_led() {
-    let led_paths:[&str; 4] = ["/sys/class/leds/led0:green:brick-status", "/sys/class/leds/led0:red:brick-status", "/sys/class/leds/led1:green:brick-status", "/sys/class/leds/led1:red:brick-status"];
-    for led in led_paths {
+    for led in LED_PATHS {
         files::write_int(led, &0);
     }
 }
 
-pub fn set_led(led: i64)
+pub fn set_led(led: usize, value: u8) {
+    let address: &str = LED_PATHS[led];
+    files::write_int(address, &(value as i64));
+}
+
