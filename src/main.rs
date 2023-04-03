2ed20106960e50db4ec99e1badd1c6800e45c422
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-04-03 16:15:43 +0200

    added methods to read battery

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod robot;
 use robot::brick;
-use std::{thread, time};
 
 fn main() {
        brick::init();
+       println!("{}", brick::max_voltage_battery());
+       println!("{}", brick::min_voltage_battery());
+       println!("{}", brick::voltage_battery());
+       println!("{}", brick::current_battery());
+       println!("{}", brick::is_LiIon());      
 }
