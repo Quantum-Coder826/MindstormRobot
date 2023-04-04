272f0efb05b36c13fac1aefe5596f33b3db6b6c3
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-04-04 15:46:32 +0200

    rollback

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod robot;
-use robot::brick;
+use robot::brick::Brick;
 use robot::sensor::Sensor;
 
 fn main() {
-       brick::init();
-
-       let sonar = Sensor::attatch(&Ports.in1);
-       println!("{}", sonar.name);
-
+       Brick::init();
+       
 }
